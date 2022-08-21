be9591a47c4645b4f393ba408ac3c5aa52ea4f5d
PolinaG <PolinaG@gmail.com>
2022-08-21 23:32:52 +0300

    Create a task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Максимальное число - {Math.Max(number1,Math.Max(number2,number3))}");
