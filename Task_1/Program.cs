6757b406168cbf084a01d97cbbee1151b1fa12cf
PolinaG <PolinaG@gmail.com>
2022-08-21 23:13:07 +0300

    Create a task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"Наибольшее число = {number1}");
+    Console.WriteLine($"Наименьшее число = {number2}");
+}
+else
+{
+    Console.WriteLine($"Наибольшее число = {number2}");
+     Console.WriteLine($"Наименьшее число = {number1}");
+}
